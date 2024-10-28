3b83c2a9e2d48990ebb9ccf94d065fde2d8346c9
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-10-27 18:50:15 -0700

    third

--- main.cpp
+++ main.cpp
@@ -13,8 +13,30 @@ using namespace std;
 int main() {
     ifstream fin("codes.txt");
     string line;
-    while ()
+    vector<string> vect;
+    list<string> lists;
+    set<string> sets;
+
+    //reading string elements-------race 1
+    while (getline(fin, line)){
+        vect.push_back(line);
+        lists.push_back(line);
+        sets.insert(line);
+    }
+
+    //sorting string elements-------race 2
+    //set is sorted by default
+    lists.sort();
+    sort(vect.begin(), vect.end());
     
+    //inserting "TESTCODE"--------race 3
+    int vectorindex = vect.size() / 2;
+    int listindex = lists.size() / 2;
+    //set will automatically insert into correct index
+
+    vect.insert(vect.begin() + vectorindex, "TESTCODE");
+
+
 
     return 0;
 }
